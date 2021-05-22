5e42d07b12203653844cd7ecdbd3c03f4a7b5e75
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-05-22 17:42:46 +0100

    Sudo cmd

---
+++ src/commands/Developer Commands/SudoCommand.ts
@@ -0,0 +1,19 @@
+import { Command, RunFunction } from '../../interfaces/Command';
+
+   export const run: RunFunction = async(client, message, args, lang) => {
+
+    if(!args.length) return client.utils.sendError(`No action provided.`, message.channel);
+    let cmd = args[0]?.toLowerCase();
+    let command: Command = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
+    if(!command) return client.utils.sendError(`Invalid command.`, message.channel);
+    try {
+        command.run(client, message, args, lang);
+    } catch (e) {
+        return client.utils.sendError(`Error: ${e}`, message.channel);
+    }
+
+    }
+export const name: string = 'sudo';
+export const category: string = 'Development';
+export const description: string = 'Bypass Command requirements for dev purposes only.';
+export const aliases: string[] = ['root'];
