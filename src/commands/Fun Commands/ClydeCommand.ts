2a717ca8c26c42a440a8cd3bd17f78e3dcb55c81
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-05-28 11:46:06 +0100

    Clyde Command

---
+++ src/commands/Fun Commands/ClydeCommand.ts
@@ -0,0 +1,24 @@
+import { RunFunction } from '../../interfaces/Command';
+
+   export const run: RunFunction = async(client, message, args) => {
+
+    if (!args[0])
+    return message.channel.send(
+      `Please specify what you want the image to say.`
+    );
+  let clydeMessage = args.join(" ");
+  if(!clydeMessage) return client.utils.sendError(`You need to provide some text.`, message.channel);
+  let encodedLink = encodeURI(
+    `https://ctk-api.herokuapp.com/clyde/${clydeMessage}`
+  );
+
+  let clydeEmbed = new client.embed().setImage(encodedLink);
+
+  message.channel.send(clydeEmbed);
+
+
+    }
+export const name: string = 'clyde';
+export const category: string = 'Fun';
+export const description: string = 'Receive an image of Clyde the system Bot saying what you like!';
+export const aliases: string[] = ['clydeimage'];
