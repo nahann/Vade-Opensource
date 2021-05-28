e7faa2b897730ea9b00f804896b6651fdc1e5117
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-05-28 12:00:41 +0100

    Gnome Command

--- src/commands/Fun Commands/GnomeCommand.ts
+++ src/commands/Fun Commands/GnomeCommand.ts
@@ -2,8 +2,21 @@ import { RunFunction } from '../../interfaces/Command';
 
    export const run: RunFunction = async(client, message, args) => {
 
+
+ 
+   
+       //get the first person they mentioned in the message
+       const user = await client.utils.getMember(message, args[0], true);
+       if(!user) return;
+
+       message.delete();
+   
+       //send a funny message in the channel to that person
+       message.channel.send(
+         `${user} Ho ho ho ha ha, ho ho ho he ha. Hello there, old chum. I’m g'not a g'nelf. I’m g'not a g'noblin. I’m a g'nome!! And you’ve been, GNOOOMED!!!`
+       );
+
     }
 export const name: string = 'gnome';
-export const category: string = ''
-export const description: string = ''
-export const aliases: string[] = ['']
+export const category: string = 'Fun';
+export const description: string = 'Cheer someone up with a lovely "gnome" message.';
