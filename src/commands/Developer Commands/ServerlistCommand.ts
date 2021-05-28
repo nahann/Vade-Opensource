f7d7340e875d946f5af6c515b0f360eb125edaa5
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-05-28 14:13:16 +0100

    Serverlist cmd

---
+++ src/commands/Developer Commands/ServerlistCommand.ts
@@ -0,0 +1,21 @@
+import { RunFunction } from '../../interfaces/Command';
+import paginationEmbed from '../../Classes/Pagination';
+
+   export const run: RunFunction = async(client, message, _args) => {
+
+    const guilds = (await client.guilds.fetch());;
+    let embed = new client.embed()
+    .setDescription(guilds.map((a) => `${a.name}: ${a.id}`))
+
+    let embeds = []
+    embeds.push(embed)
+
+    let emojiList = ["◀️", "▶️"];
+    return paginationEmbed(message, embeds, emojiList, 60000)
+
+    }
+export const name: string = 'serverlist';
+export const category: string = 'Development';
+export const description: string = 'List the Bots servers.';
+export const aliases: string[] = ['slist'];
+export const devOnly: boolean = true;
