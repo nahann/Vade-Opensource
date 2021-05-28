be4c9657b9ca2a5cae559a9e25d7f348dda321cb
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-05-28 12:16:00 +0100

    Dadjoke Command

---
+++ src/commands/Fun Commands/DadjokeCommand.ts
@@ -0,0 +1,14 @@
+import { RunFunction } from '../../interfaces/Command';
+import getJoke from 'give-me-a-joke';
+
+   export const run: RunFunction = async(_client, message, _args) => {
+
+    getJoke.getRandomDadJoke(function (joke) {
+        return message.channel.send(joke);
+      });
+
+    }
+export const name: string = 'dadjoke';
+export const category: string = 'Fun';
+export const description: string = 'Have a random dad joke sent into the chat!';
+export const aliases: string[] = ['badjoke'];
