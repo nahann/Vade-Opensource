5e1dd97fd46eb0a8a5d45c6baf8347d7f90e018d
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-05-28 12:12:06 +0100

    Cuddle Command

---
+++ src/commands/Fun Commands/CuddleCommand.ts
@@ -0,0 +1,23 @@
+import { RunFunction } from '../../interfaces/Command';
+
+   export const run: RunFunction = async(client, message, args) => {
+
+    const user = await client.utils.getMember(message, args[0], true);
+    if(!user) return;
+
+    message.channel.send(
+        `${user} You got a cuddle from ${message.author.username} ❤`,
+        {
+          embed: {
+            image: {
+              url: "https://i.imgur.com/0yAIWbg.gif",
+            },
+          },
+        }
+      );
+
+    }
+export const name: string = 'cuddle';
+export const category: string = 'Fun';
+export const description: string = 'Snuggle up next to someone of your choice!';
+export const aliases: string[] = ['snuggle'];
