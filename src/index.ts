b9bc1bf991999b1de92b77f9df55af904bac22b3
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-05-29 22:48:49 +0100

    2Ds shit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import "discord.js-light";
-import { Config } from "./interfaces/Config";
 import * as File from "./config.json";
 import { Bot } from "./client/Client";
 
+import type { Config } from "./interfaces/Config";
+
+process.on("unhandledRejection", (err) => {
+    console.log(err)
+})
+
 new Bot({}).start(File as Config);
+
--- src/utils/Lavaclient.ts
+++ src/utils/Lavaclient.ts
@@ -1,8 +1,54 @@
-import {  } from "lavaclient";
-
-export namespace lavaclient {
-
-
-    
-
-}
+// import { EventEmitter } from "events";
+// import { decode } from "@lavalink/encoding"
+// import { Manager, SocketData, Player } from "lavaclient";
+// import { LAVA_HOST, LAVA_PASSWORD, LAVA_PORT } from"../config.json"
+//
+// import type { Track } from "@lavaclient/types";
+// import type { Bot } from "../client/Client";
+//
+// export namespace lavaclient {
+//
+//     const nodes: SocketData[] = [
+//         {
+//             id: "A",
+//             host: LAVA_HOST,
+//             port: LAVA_PORT,
+//             password: LAVA_PASSWORD
+//         }
+//     ]
+//
+//     let manager: Manager
+//
+//     export async function connect(bot: Bot) {
+//         manager = new Manager(nodes, {
+//             send: (id, payload) => bot.guilds.forge(id).shard.send(payload),
+//             userId: bot.user.id
+//         });
+//     }
+//
+//     class Queue extends EventEmitter {
+//         readonly player: Player;
+//
+//         tracks: string[] = [];
+//
+//         constructor(player: Player) {
+//             super();
+//
+//             this.player = player;
+//             this.player.on("end", ({ reason, track }) => {
+//                 this._end(reason, decode(track));
+//             })
+//         }
+//
+//
+//
+//         add(tracks: Track | Track[] | string) {
+//
+//         }
+//
+//         _end(reason: "TrackEndReason", track: Track) {
+//         }
+//
+//     }
+//
+// }
