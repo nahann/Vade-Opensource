c147865ef56cc51f9dd7b7791de268430d3e3f8f
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-05-29 23:49:54 +0100

    Playlist schema

---
+++ src/models/Users/playlist.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+export interface IPLaylist extends mongoose.Document {
+    username: string;
+    userID: string;
+    playlistName: boolean;
+    playlistArray: array<string>;
+}
+
+const reqString = {
+    type: String,
+    required: true,
+};
+const reqArray = {
+    type: Array,
+    required: true,
+};
+
+
+const guildSchema = new mongoose.Schema({
+    username: String,
+    userID: reqString,
+    playlistName: reqString,
+    playlistArray: reqArray,
+});
+
+const guilds = mongoose.model<IPLaylist>("playlists", guildSchema);
+
+export default guilds;
