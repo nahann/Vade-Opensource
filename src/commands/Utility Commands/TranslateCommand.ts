1fd634e10adee75c5369fbf2c78d67ad40498413
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-05-30 15:23:42 +0100

    Translate Command

---
+++ src/commands/Utility Commands/TranslateCommand.ts
@@ -0,0 +1,35 @@
+import type { RunFunction } from '../../interfaces/Command';
+import translate from '@iamtraction/google-translate'
+
+export const run: RunFunction = async(client, message, args) => {
+
+    try {
+        if (args.length < 2) {
+            return client.utils.sendError(
+                "Command Usage: `translate <Language> <Text>`", message.channel
+            );
+        }
+
+        const result = await translate(args.slice(1).join(" "), { to: args[0] });
+
+        const embed = new client.embed()
+            .setColor("#68b64a")
+            .setDescription(`Original text: ${args.slice(1).join(" ")} \n\nTranslation: ${result.text}`)
+            .setFooter(
+                `Translation from ${result.from.language.iso.toUpperCase()} to ${args[0].toUpperCase()}`
+            );
+        message.channel.send({ embed });
+    } catch (err) {
+        return client.utils.sendError(
+            `Oh no, an error occurred: \`${err.message}\`.`, message.channel
+        );
+    }
+
+
+
+}
+
+export const name: string = 'translate';
+export const category: string = ''
+export const description: string = ''
+export const aliases: string[] = ['translation']
