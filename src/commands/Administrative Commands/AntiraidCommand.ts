2edf226517a586228cf2fc7078c034adcdc5accc
Ethan <68427544+V1rtuaI@users.noreply.github.com>
2021-05-30 01:12:43 +0100

    Begin anti raid system

---
+++ src/commands/Administrative Commands/AntiraidCommand.ts
@@ -0,0 +1,10 @@
+import type { RunFunction } from '../../interfaces/Command';
+
+export const run: RunFunction = async(client, message, args) => {
+
+}
+
+export const name: string = 'antiraid';
+export const category: string = 'Administrative';
+export const description: string = '';
+export const userPerms: string[] = ['MANAGE_GUILD'];
